b0a9d5c4621c01c14359a033e4b2f4821b7ef8da
Ngort <nicholasgort@gmail.com>
2016-07-05 16:48:51 -0500

    Program adds column "Start". Both start and end functions defined
    Notfound.tsv compiles all not found peptides

--- Peptide_count/parseproteome.py
+++ Peptide_count/parseproteome.py
@@ -5,21 +5,17 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
-#import experimental results from LC/MS
-data = pd.read_csv("Hs_helaN_1003.pep_list_FDR0010",sep='\t',names=['Protein','Fracti    on','Peptide','Count'],index_col=['Protein','Peptide']).sortlevel(1)
-
-#import proteome from ENSEMBL FTP database, parse it with BioPython
 proteome = list(SeqIO.parse("Homo_sapiens.GRCh38.pep.all.fa","fasta"))
 
+#filter proteins whose ID ends in 1
 unique = [protein for protein in proteome if protein.id[-1]=='1']
 
-
+#delete the .1
 for protein in unique:
 	protein.id = protein.id[:-2]
 	print protein.id
 
-print proteome[0]
-print unique[0]
 
+#write new FASTA file
 SeqIO.write(unique,'unique_proteins.fasta',"fasta")
 
